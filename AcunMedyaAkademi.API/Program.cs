9a20b9bcc797ab69f1b6af02ad3c7a14956656bb
senaydn <senaaydin04@gmail.com>
2025-02-01 23:56:28 +0300

    Created CheckAgeFromDirectory method

---
+++ AcunMedyaAkademi.API/Program.cs
@@ -0,0 +1,61 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Kullanıcının yaşını alarak ehliyet alıp alamayacağını kontrol eder.
+app.MapGet("/CheckAgeValidation", (int age) =>
+    {
+        if (age >= 18)
+        {
+            return Task.FromResult("Ehliyet alabilirsiniz.");
+        }
+        else
+        {
+            return Task.FromResult("Ehliyet alamazsınız.");
+        }
+    })
+    .WithName("CheckAgeValidation")
+    .WithOpenApi();
+
+// Masaüstünde "ageInfo.txt" adında bir dosya oluşturur ve yaşı içerisinden okuyarak döner.
+app.MapPost("/CheckAgeFromDirectory", async (HttpContext context, int age) =>
+{
+    var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "ageInfo.txt");
+
+    // Dosyaya yazma işlemi
+    await File.WriteAllTextAsync(filePath, age.ToString());
+
+    // Dosyadan okuma işlemi
+    string fileContent = await File.ReadAllTextAsync(filePath);
+
+    // Yaşı kontrol etme
+    if (int.TryParse(fileContent, out int storedAge))
+    {
+        if (storedAge >= 18)
+        {
+            return Results.Ok("Ehliyet alabilirsiniz.");
+        }
+        else
+        {
+            return Results.BadRequest("Ehliyet alamazsınız.");
+        }
+    }
+
+    return Results.BadRequest("Geçerli bir yaş değeri okunamadı.");
+});
+
+app.Run();
